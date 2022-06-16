d9139a997885437f759c3885d73b76d181e75f12
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-06-16 14:55:27 +0300

    addcontent with  styles to MaterialPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCurrentUser } from './redux/auth/auth-operation';
-import AuthPage from './pages/AuthPage/AuthPage';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { getCurrentUser } from './redux/auth/auth-operation';
+
+// import AuthPage from './pages/AuthPage/AuthPage';
+// import MainPage from './pages/MainPage';
+import MaterialsPage from 'pages/MaterialsPage';
+
 export const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCurrentUser());
-  }, [dispatch]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getCurrentUser());
+  // }, [dispatch]);
   return (
     <>
-      <AuthPage />
+      {/* <AuthPage />
+      <MainPage /> */}
+      <MaterialsPage />
     </>
   );
 };
