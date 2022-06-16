2dedef9394361ab3bf2fea01e643ad365caf881c
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-06-16 16:12:02 +0300

    uncomment auth page into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { getCurrentUser } from './redux/auth/auth-operation';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCurrentUser } from './redux/auth/auth-operation';
 
-// import AuthPage from './pages/AuthPage/AuthPage';
-// import MainPage from './pages/MainPage';
+import AuthPage from './pages/AuthPage/AuthPage';
+import MainPage from './pages/MainPage';
 import MaterialsPage from 'pages/MaterialsPage';
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(getCurrentUser());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
   return (
     <>
-      {/* <AuthPage />
-      <MainPage /> */}
+      <AuthPage />
+      <MainPage />
       <MaterialsPage />
     </>
   );
