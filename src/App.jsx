4810a36f1727d41b8c81f6ad7548da9e2b365592
Andari757 <matviyskrypka@gmail.com>
2022-06-17 15:17:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCurrentUser } from './redux/auth/auth-operation';
-import NavigationRoutes from "./Routes"
+
 import LayOut from 'components/LayOut';
 import AuthPage from './pages/AuthPage/AuthPage';
 import MainPage from './pages/MainPage';
@@ -10,9 +10,16 @@ import TestPage from './pages/TestPage';
 import MaterialsPage from 'pages/MaterialsPage';
 import ResultPage from 'pages/ResultPage';
 export const App = () => {
+  const dispatch = useDispatch(); useEffect(() => { dispatch(getCurrentUser()); }, [dispatch]);
+
   return (
-
-    <NavigationRoutes />
-
+    <>
+      <LayOut />
+      <AuthPage />
+      <MainPage />
+      <TestPage />
+      <MaterialsPage />
+      <ResultPage />
+    </>
   );
 };
