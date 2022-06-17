5b0c96c00cfe5c8805278fb2fbace22276287ab0
Andari757 <matviyskrypka@gmail.com>
2022-06-17 15:25:03 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,12 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCurrentUser } from './redux/auth/auth-operation';
 
-import LayOut from 'components/LayOut';
-import AuthPage from './pages/AuthPage/AuthPage';
-import MainPage from './pages/MainPage';
-import TestPage from './pages/TestPage';
+import NavigationRoutes from 'Routes';
 
-import MaterialsPage from 'pages/MaterialsPage';
-import ResultPage from 'pages/ResultPage';
 export const App = () => {
   const dispatch = useDispatch(); useEffect(() => { dispatch(getCurrentUser()); }, [dispatch]);
 
   return (
-    <>
-      <LayOut />
-      <AuthPage />
-      <MainPage />
-      <TestPage />
-      <MaterialsPage />
-      <ResultPage />
-    </>
+    <NavigationRoutes />
   );
 };
