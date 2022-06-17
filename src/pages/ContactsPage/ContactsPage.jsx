c3bfecc8ab1bd381472895bd49d403e5f12e94a9
Dmytro Kulakov <gambler11kulakov@gmail.com>
2022-06-17 18:00:53 +0300

    contacts-page

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,46 +1,36 @@
-import PropTypes from 'prop-types';
-
-import ContactItem from './ContactItem';
+import data from './dataContacts.json';
 
 import s from './contactsPage.module.css';
 
-const ContactsPage = ({ contacts }) => {
+function ContactsPage() {
+  const contactItem = data.map(
+    ({ id, avatar, name, surename, posn, descr }) => {
+      return (
+        <li key={id} className={s.card}>
+          <div className={s.imgWrap}>
+            <img src={avatar} alt="" className={s.img} />
+            <p className={s.description}>{descr}</p>
+          </div>
+          <div className={s.descriptionContainer}>
+            <h2 className={s.name}>
+              {name} <br /> {surename}
+            </h2>
+            <p className={s.position}>{posn}</p>
+          </div>
+        </li>
+      );
+    }
+  );
+
   return (
     <div className={s.contactPage}>
       <div className={s.wrap}></div>
       <div className={s.box}>
         <h1 className={s.title}>Our team</h1>
       </div>
-      <ul className={s.list}>
-        {contacts.map(item => (
-          <ContactItem
-            key={item.id}
-            avatar={item.avatar}
-            name={item.name}
-            descr={item.descr}
-            posn={item.posn}
-            surename={item.surename}
-          />
-        ))}
-      </ul>
+      <ul className={s.list}>{contactItem}</ul>
     </div>
   );
-};
+}
 
 export default ContactsPage;
-
-ContactsPage.defaultProps = {
-  contacts: [],
-};
-
-ContactsPage.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      posn: PropTypes.bool.isRequired,
-      descr: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
-};
