66a4542614768363a8e72044fcf9c98079ca02f9
Dmytro Kulakov <gambler11kulakov@gmail.com>
2022-06-17 14:31:04 +0300

    contacts-page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from './redux/auth/auth-operation';
 import AuthPage from './pages/AuthPage/AuthPage';
 import MainPage from './pages/MainPage';
 import MaterialsPage from 'pages/MaterialsPage';
+import ContactsPage from 'pages/ContactsPage';
+import contacts from './pages/ContactsPage/dataContacts.json';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -16,6 +18,7 @@ export const App = () => {
       <AuthPage />
       <MainPage />
       <MaterialsPage />
+      <ContactsPage contacts={contacts} />
     </>
   );
 };
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,13 +1,46 @@
-function ContactsPage() {
+import PropTypes from 'prop-types';
+
+import ContactItem from './ContactItem';
+
+import s from './contactsPage.module.css';
+
+const ContactsPage = ({ contacts }) => {
   return (
-    <>
-      {/* todo 
-      gallery with developers
-      ! optional modal window with big photo
-      
-      */}
-    </>
+    <div className={s.contactPage}>
+      <div className={s.wrap}></div>
+      <div className={s.box}>
+        <h1 className={s.title}>Our team</h1>
+      </div>
+      <ul className={s.list}>
+        {contacts.map(item => (
+          <ContactItem
+            key={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            descr={item.descr}
+            posn={item.posn}
+            surename={item.surename}
+          />
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default ContactsPage;
+
+ContactsPage.defaultProps = {
+  contacts: [],
+};
+
+ContactsPage.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      posn: PropTypes.bool.isRequired,
+      descr: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
